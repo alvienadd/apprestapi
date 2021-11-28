66485f64a441559f7159e111106cd6ca50fb80a2
Alvien Laksono <alvien.add@gmail.com>
2021-11-28 15:19:05 +0700

    Berhasil menampilkan data berdasarkan ID

--- controller.js
+++ controller.js
@@ -11,9 +11,25 @@ exports.index = function (req, res) {
 exports.tampilsemuamahasiswa = function (req, res) {
   connection.query("SELECT * FROM mahasiswa", function (error, rows, fields) {
     if (error) {
-      console.log(error0);
+      console.log(error);
     } else {
       response.ok(rows, res);
     }
   });
+};
+
+//menampilkan semua data berdasarkan ID
+exports.tampilberdasarkanId = function (req, res) {
+  let id = req.params.id;
+  connection.query(
+    "SELECT * FROM mahasiswa WHERE id_mahasiswa=?",
+    [id],
+    function (error, rows, fields) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.ok(rows, res);
+      }
+    }
+  );
 };
